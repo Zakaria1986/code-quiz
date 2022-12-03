f7e41d6ae16cd9f56c6b38a472e4483f6ae8df40
Zak khan <zakaria2012@gmail.com>
2022-12-03 11:35:17 +0000

    Pushing user requirement in humanly reable text, which next will start writing some sudo code for

---
+++ assets/js/questions.js
@@ -0,0 +1,39 @@
+// AS A coding boot camp student
+// I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+// SO THAT I can gauge my progress compared to my peers
+
+
+
+// Acceptance Criteria:
+    // GIVEN I am taking a code quiz
+    // WHEN I click the start button
+    // THEN a timer starts and I am presented with a question
+    // WHEN I answer a question
+    // THEN I am presented with another question
+    // WHEN I answer a question incorrectly
+    // THEN time is subtracted from the clock
+    // WHEN all questions are answered or the timer reaches 0
+    // THEN the game is over
+    // WHEN the game is over
+    // THEN I can save my initials and score
+
+
+
+    
+    // 1. Grab the start button from index.html: document.querySeelctor("button name") then store in a variable
+    // 2.  On click methods: addEventListener('click', function(){}); 
+    // 3. write some questions: Use dom methods to create and store series of question then appand to the dom, which would go inside the addEventListner function
+    // 4. Write a timer function: that start as the button is clicked;  
+    // 4. Write so conditional statement to check:
+        // - if the the user selects the wirte answer present the next question 
+        // - else/else if deduct time from the timer and resent the net question
+        // if there is no question left or the timer has reached 0, end the game. 
+    // 5. Ask user to enter their initials:
+        // create form in the form insert input box 
+        // then take the value store it in browser local storage or in an array or an object 
+        // get the information and present it to the user 
+  
+
+
+
+
