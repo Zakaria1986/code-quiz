a8100182c5e8b377476e4d55b1813ff8e17fa4d1
Zak khan <zakaria2012@gmail.com>
2022-12-03 18:54:15 +0000

    Updating countDown

---
+++ assets/js/logic.js
@@ -0,0 +1,37 @@
+// 1. Grab the start button from index.html: document.querySeelctor("button name") then store in a variable
+// 2.  On click methods: addEventListener('click', function(){}); 
+// 3. write some questions: Use dom methods to create and store series of question then appand to the dom, which would go inside the addEventListner function
+// 4. Write a timer function: that start as the button is clicked;  
+// 4. Write so conditional statement to check:
+// - if the the user selects the wirte answer present the next question 
+// - else/else if deduct time from the timer and resent the net question
+// if there is no question left or the timer has reached 0, end the game. 
+// 5. Ask user to enter their initials:
+// create form in the form insert input box 
+// then take the value store it in browser local storage or in an array or an object 
+// get the information and present it to the user 
+
+
+// short hand of document select
+var doc = document;
+
+// Getting the start element id
+var btnStart = doc.getElementById('start');
+
+// countDown function from 60 to 0
+function countDown() {
+    var timerSec = 60;
+    var interVal = setInterval(function () {
+        var timerUpdate = doc.querySelector('#time');
+        timerUpdate.innerText = timerSec;
+
+        if (timerSec == 0) {
+            clearInterval(interVal);
+        };
+        timerSec--;
+    }, 1000
+    );
+}
+
+// When start button is click countDown function gets regards and the function is passed by reference
+btnStart.addEventListener('click', countDown);
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -5,34 +5,47 @@
 
 
 // Acceptance Criteria:
-    // GIVEN I am taking a code quiz
-    // WHEN I click the start button
-    // THEN a timer starts and I am presented with a question
-    // WHEN I answer a question
-    // THEN I am presented with another question
-    // WHEN I answer a question incorrectly
-    // THEN time is subtracted from the clock
-    // WHEN all questions are answered or the timer reaches 0
-    // THEN the game is over
-    // WHEN the game is over
-    // THEN I can save my initials and score
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and score
+
+
+// 1. Grab the start button from index.html: document.querySeelctor("button name") then store in a variable
+// 2.  On click methods: addEventListener('click', function(){}); 
+// 3. write some questions: Use dom methods to create and store series of question then appand to the dom, which would go inside the addEventListner function
+// 4. Write a timer function: that start as the button is clicked;  
+// 4. Write so conditional statement to check:
+// - if the the user selects the wirte answer present the next question 
+// - else/else if deduct time from the timer and resent the net question
+// if there is no question left or the timer has reached 0, end the game. 
+// 5. Ask user to enter their initials:
+// create form in the form insert input box 
+// then take the value store it in browser local storage or in an array or an object 
+// get the information and present it to the user 
 
 
 
-    
-    // 1. Grab the start button from index.html: document.querySeelctor("button name") then store in a variable
-    // 2.  On click methods: addEventListener('click', function(){}); 
-    // 3. write some questions: Use dom methods to create and store series of question then appand to the dom, which would go inside the addEventListner function
-    // 4. Write a timer function: that start as the button is clicked;  
-    // 4. Write so conditional statement to check:
-        // - if the the user selects the wirte answer present the next question 
-        // - else/else if deduct time from the timer and resent the net question
-        // if there is no question left or the timer has reached 0, end the game. 
-    // 5. Ask user to enter their initials:
-        // create form in the form insert input box 
-        // then take the value store it in browser local storage or in an array or an object 
-        // get the information and present it to the user 
-  
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
